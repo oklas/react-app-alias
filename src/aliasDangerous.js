cfb7349d935deae1176f5110ed88c4f3703c5329
Serguei Okladnikov <oklaspec@gmail.com>
2020-06-04 00:19:17 +0300

    feat(aliasDangerous): add alias dangerous

---
+++ src/aliasDangerous.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const paths = require('react-scripts/config/paths')
+const {expandResolveAlias, expandPluginsScope} = require('./index')
+
+const eslintLoaderModule = require.resolve('eslint-loader')
+
+function isRuleOfEslint(rule) {
+  if(eslintLoaderModule === rule.loader) return true
+  if(rule.use && 0 < rule.use.filter(isRuleOfEslint)) return true
+  return false
+}
+
+function isLocalPath(parent) {
+  return function(dir) {
+    const relative = path.relative(parent, dir)
+    return relative && !relative.startsWith('..') && !path.isAbsolute(relative)
+  }
+}
+
+function updateInclude(rule, include) {
+  rule.include = (
+    isRuleOfEslint(rule) ? include.filter(isLocalPath(paths.appSrc)) : include
+  ).concat(rule.include)
+}
+
+function expandRulesInclude(rules, include) {
+  rules.forEach(function(rule) {
+    if(rule.include === paths.appSrc) {
+      updateInclude(rule, include)
+    }
+    if(rule.oneOf)
+      expandRulesInclude(rule.oneOf, include)
+  })
+}
+
+function aliasDangerous(aliasMap) {
+  const aliasLocal = Object.keys(aliasMap).reduce( (a,i) => {
+    a[i] = path.resolve(paths.appPath, aliasMap[i])
+    return a
+  }, {})
+  return function(config) {
+    expandResolveAlias(config.resolve, aliasLocal)
+    expandRulesInclude(config.module.rules, Object.values(aliasLocal))
+    expandPluginsScope(config.resolve.plugins, Object.values(aliasLocal))
+    return config
+  }
+}
+
+module.exports = {
+  aliasDangerous,
+}
