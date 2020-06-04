692ca8024543334e22540dd033db1d590bd26da7
JollyGoodHolly <64430232+JollyGoodHolly@users.noreply.github.com>
2020-06-04 14:02:45 +0200

    rev

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function expandResolveAlias(resolve, alias) {
   resolve.alias = Object.assign(resolve.alias || {}, alias)
 }
 
-function expandRulesInclude(rules, include, ignoreForEslint) {
-  rules.forEach(function (rule) {
-    if (rule.include === paths.appSrc && !ignoreForEslint)
+function expandRulesInclude(rules, include) {
+  rules.forEach(function(rule) {
+    if(rule.include === paths.appSrc)
       rule.include = include.concat(rule.include)
-    if (rule.oneOf)
-      expandRulesInclude(rule.oneOf, include, false)
+    if(rule.oneOf)
+      expandRulesInclude(rule.oneOf, include)
   })
 }
 
@@ -32,20 +32,20 @@ function expandPluginsScope(plugins, dirs, files) {
   const pluginPos = plugins
     .map(x => x.constructor.name)
     .indexOf(pluginName)
-  if (pluginPos !== -1) {
+  if(pluginPos !== -1) {
     plugins.splice(pluginPos, 1)
     plugins[pluginPos] = new ModuleScopePlugin(dirs, files)
   }
 }
 
-function alias(aliasMap, ignoreForEslint = false) {
-  const aliasLocal = Object.keys(aliasMap).reduce((a, i) => {
+function alias(aliasMap) {
+  const aliasLocal = Object.keys(aliasMap).reduce( (a,i) => {
     a[i] = path.resolve(paths.appPath, aliasMap[i])
     return a
   }, {})
-  return function (config) {
+  return function(config) {
     expandResolveAlias(config.resolve, aliasLocal)
-    expandRulesInclude(config.module.rules, Object.values(aliasLocal), ignoreForEslint)
+    expandRulesInclude(config.module.rules, Object.values(aliasLocal))
     expandPluginsScope(config.resolve.plugins, Object.values(aliasLocal))
     return config
   }
@@ -55,28 +55,28 @@ function configPaths(configPath = '') {
   const confPath = (
     configPath.length > 0 && fs.existsSync(path.resolve(paths.appPath, configPath)) ?
       path.resolve(paths.appPath, configPath) :
-      fs.existsSync(path.resolve(paths.appPath, 'tsconfig.json')) ?
-        path.resolve(paths.appPath, 'tsconfig.json') :
-        fs.existsSync(path.resolve(paths.appPath, 'jsconfig.json')) ?
-          path.resolve(paths.appPath, 'jsconfig.json') :
-          ''
+    fs.existsSync(path.resolve(paths.appPath, 'tsconfig.json')) ?
+      path.resolve(paths.appPath, 'tsconfig.json') :
+    fs.existsSync(path.resolve(paths.appPath, 'jsconfig.json')) ?
+      path.resolve(paths.appPath, 'jsconfig.json') :
+    ''
   )
 
-  if (!confPath)
-    throw Error('react-app-rewire-alias:configPaths: no config file found')
+  if(!confPath)
+    throw Error('react-app-rewire-alias:configPaths: there is no config file found')
 
   const conf = require(confPath)
 
-  if (typeof conf.compilerOptions.paths !== 'object')
+  if(typeof conf.compilerOptions.paths !== 'object' )
     throw Error('react-app-rewire-alias:configPaths: array expected for paths')
 
-  if (!conf.compilerOptions || !conf.compilerOptions.paths)
+  if(!conf.compilerOptions || !conf.compilerOptions.paths)
     return {}
 
-  return Object.keys(conf.compilerOptions.paths).reduce((a, path) => {
+  return Object.keys(conf.compilerOptions.paths).reduce( (a, path) => {
     const value = conf.compilerOptions.paths[path]
     const target = Array.isArray(value) ? value[0] : value
-    a[path.replace(/\/\*$/, '')] = target.replace(/\/\*$/, '')
+    a[path.replace(/\/\*$/,'')] = target.replace(/\/\*$/,'')
     return a
   }, {})
 }
