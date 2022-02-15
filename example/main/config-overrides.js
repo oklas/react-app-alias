c9a6d38d0fe0a650d7617428aefef5903863abad
Serguei Okladnikov <oklaspec@gmail.com>
2022-02-15 19:07:05 +0300

    fix: function `alias` renamed to `aliasWebpack` #60
    BREAKING CHANGE: The `alias` function has been renamed to `aliasWebpack` to improve the readability of the rewire API, as part of a major release along with a rename of the library itself.

--- example/main/config-overrides.js
+++ example/main/config-overrides.js
@@ -1,6 +1,6 @@
-const {alias, aliasJest, configPaths} = require('react-app-alias')
+const {aliasWebpack, aliasJest, configPaths} = require('react-app-alias')
 
 const aliasPaths = configPaths('./tsconfig.paths.json')
 
-module.exports = alias(aliasPaths)
+module.exports = aliasWebpack(aliasPaths)
 module.exports.jest = aliasJest(aliasPaths)
