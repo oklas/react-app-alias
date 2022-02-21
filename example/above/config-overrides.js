271b506c15c779ad2854d072b395985cfe2f51c8
Serguei Okladnikov <oklaspec@gmail.com>
2022-02-21 19:25:04 +0300

    build: replace rewire with craco for one example

--- example/above/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const {aliasWebpack, aliasJest, configPaths} = require('react-app-alias-ex')
-
-const aliasPaths = configPaths('./tsconfig.paths.json')
-
-module.exports = aliasWebpack(aliasPaths)
-module.exports.jest = aliasJest(aliasPaths)
