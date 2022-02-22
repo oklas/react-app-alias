930ee59e7274a4e911d7cce024a744c9bd8710e7
Serguei Okladnikov <oklaspec@gmail.com>
2022-02-22 17:44:59 +0300

    feat: process config file form extends section of tsconfig #59

--- packages/react-app-alias/__tests__/react-app-alias.test.js
+++ packages/react-app-alias/__tests__/react-app-alias.test.js
@@ -1,7 +1,16 @@
 'use strict';
-
-const reactAppAlias = require('../src');
+const path = require('path')
+const {configPathsRaw} = require('../src');
 
 describe('react-app-alias', () => {
   test.todo('tested by tests in projects in example folder');
 });
+
+describe('extends section of tsconfig on detect config file stage', () => {
+  test('read both file and extends', () => {
+    const paths = configPathsRaw(path.resolve(__dirname, './59-tsconfig.json'))
+    expect(paths['alias/*'][0]).toBe('target/*');
+    expect(paths['alias-paths/*'][0]).toBe('target-paths/*');
+  });
+});
+
